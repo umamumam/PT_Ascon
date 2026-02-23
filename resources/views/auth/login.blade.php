2d87f041638b07db61bcf4c8813114b8656823eb
umamumam <miftahulumam862@gmail.com>
2026-02-23 13:28:29 +0700

    Up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,7 +87,7 @@
 
             <div class="d-flex col-12 col-lg-4 align-items-center authentication-bg p-sm-12 p-6">
                 <div class="w-px-400 mx-auto mt-12 pt-5">
-                    <h4 class="mb-1">Welcome to Vuexy! 👋</h4>
+                    <h4 class="mb-1">Welcome to Ascon! 👋</h4>
                     <p class="mb-6">Please sign-in to your account and start the adventure</p>
 
                     <form id="formAuthentication" class="mb-6" action="{{ route('login') }}" method="POST">
