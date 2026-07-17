9b878ca8c8e086e73f9bdfaf547c96f556cb3c5a
umamumam <miftahulumam862@gmail.com>
2026-07-17 16:14:23 +0700

    feat: add landing page layout template with custom navigation and hero section styling

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -374,9 +374,9 @@
                         <li class="nav-item">
                             <a class="nav-link fw-medium" aria-current="page" href="/about">About Us</a>
                         </li>
-                        {{-- <li class="nav-item">
-                            <a class="nav-link fw-medium" href="/business">Business</a>
-                        </li> --}}
+                        <li class="nav-item">
+                            <a class="nav-link fw-medium" href="/services">Services</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle fw-medium" href="#" id="navbarDropdown" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
